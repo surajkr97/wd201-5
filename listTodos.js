2478e801bea1594f6c823a83b581b4379cf2b388
Suraj kumar <surajkumar06174@gmail.com>
2022-12-28 21:07:38 +0530

    Create listTodos.js

---
+++ listTodos.js
@@ -0,0 +1,13 @@
+//  listTodos.js
+const db = require("./models/index");
+
+const listTodo = async () => {
+  try {
+    await db.Todo.showList();
+  } catch (error) {
+    console.error(error);
+  }
+};
+(async () => {
+  await listTodo();
+})();
